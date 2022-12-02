f54d854c3694974bacb4c24afa68e842b3968d53
Mallory Allen <mallorytypes@gmail.com>
2022-12-02 17:14:31 -0500

    day 2

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 <!--SOLUTIONS-->
 
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 ![Day](https://badgen.net/badge/03/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
@@ -69,16 +69,16 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.47ms
-Time part 2: 0.521ms
-Both parts: 0.9908000000000001ms
+Time part 1: 0.521ms
+Time part 2: 0.513ms
+Both parts: 1.0341ms
 ```
 
 ```
 Day 02
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.983ms
+Time part 2: 2.784ms
+Both parts: 5.7669ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 0.991ms
+Total stars: 4/50
+Total time: 6.801ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day02/index.js
@@ -0,0 +1,117 @@
+import run from "aocrunner";
+import { sum } from "../utils.js";
+
+const RPS = {
+  rock: {
+    tiesWith: "rock",
+    beats: "scissors",
+    losesTo: "paper",
+    score: 1,
+  },
+  paper: {
+    tiesWith: "paper",
+    beats: "rock",
+    losesTo: "scissors",
+    score: 2,
+  },
+  scissors: {
+    tiesWith: "scissors",
+    beats: "paper",
+    losesTo: "rock",
+    score: 3,
+  },
+};
+
+const SCORE_LOSS = 0;
+const SCORE_TIE = 3;
+const SCORE_WIN = 6;
+
+const OPPONENT_GLYPH_MAPPING = {
+  A: "rock",
+  B: "paper",
+  C: "scissors",
+};
+
+const MY_GLYPH_MAPPING = {
+  X: "rock",
+  Y: "paper",
+  Z: "scissors",
+};
+
+const INTENDED_OUTCOME_MAPPING = {
+  X: "lose",
+  Y: "tie",
+  Z: "win",
+};
+
+const INTENDED_OUTCOME_PICK_MAPPING = {
+  lose: "beats", // pick the one the opponent beats to lose
+  tie: "tiesWith",
+  win: "losesTo", // pick the one the opponent loses to to win
+};
+
+function score(myShape, opponentShape) {
+  let resultScore = SCORE_TIE;
+
+  if (RPS[myShape].beats == opponentShape) {
+    resultScore = SCORE_WIN;
+  } else if (RPS[myShape].losesTo == opponentShape) {
+    resultScore = SCORE_LOSS;
+  }
+
+  return RPS[myShape].score + resultScore;
+}
+
+const parseInput = (rawInput) => rawInput.split("\n");
+
+const part1 = (rawInput) => {
+  const rounds = parseInput(rawInput);
+
+  return rounds
+    .map((round) => {
+      const [opponentGlyph, myGlyph] = round.split(" ");
+      return [OPPONENT_GLYPH_MAPPING[opponentGlyph], MY_GLYPH_MAPPING[myGlyph]];
+    })
+    .map(([opponentShape, myShape]) => score(myShape, opponentShape))
+    .reduce(sum);
+};
+
+const part2 = (rawInput) => {
+  const rounds = parseInput(rawInput);
+
+  return rounds
+    .map((round) => {
+      const [opponentGlyph, myGlyph] = round.split(" ");
+      const intendedOutcome = INTENDED_OUTCOME_MAPPING[myGlyph];
+      const opponentsShape = OPPONENT_GLYPH_MAPPING[opponentGlyph];
+
+      const iChoose =
+        RPS[opponentsShape][INTENDED_OUTCOME_PICK_MAPPING[intendedOutcome]];
+
+      return [opponentsShape, iChoose];
+    })
+    .map(([opponentShape, myShape]) => score(myShape, opponentShape))
+    .reduce(sum);
+};
+
+run({
+  part1: {
+    tests: [],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `
+        A Y
+        B X
+        C Z
+        `,
+        expected: 12,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
